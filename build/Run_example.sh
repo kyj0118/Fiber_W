bf98f594063229836497a120710d3ed0e37a37c8
kyj0118 <69233770+kyj0118@users.noreply.github.com>
2021-07-13 15:13:31 +0900

    Add files via upload

---
+++ build/Run_example.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# ./exampleB5 [1. macro file] [2. output root file name] [3. random seed]    or   ./exampleB5    for vis.mac
+
+#./exampleB5 RandomGeneration.mac random_test 1
+./exampleB5 GPSGeneration.mac gps_test 1
+
