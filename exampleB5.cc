9b16012e965a8ab8a712861ddf6576ac928e8d49
kyj0118 <rladudwns118@korea.ac.kr>
2021-07-13 15:14:47 +0900

    First commit

---
+++ exampleB5.cc
@@ -0,0 +1,161 @@
+//
+// ********************************************************************
+// * License and Disclaimer                                           *
+// *                                                                  *
+// * The  Geant4 software  is  copyright of the Copyright Holders  of *
+// * the Geant4 Collaboration.  It is provided  under  the terms  and *
+// * conditions of the Geant4 Software License,  included in the file *
+// * LICENSE and available at  http://cern.ch/geant4/license .  These *
+// * include a list of copyright holders.                             *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.  Please see the license in the file  LICENSE  and URL above *
+// * for the full disclaimer and the limitation of liability.         *
+// *                                                                  *
+// * This  code  implementation is the result of  the  scientific and *
+// * technical work of the GEANT4 collaboration.                      *
+// * By using,  copying,  modifying or  distributing the software (or *
+// * any work based  on the software)  you  agree  to acknowledge its *
+// * use  in  resulting  scientific  publications,  and indicate your *
+// * acceptance of all terms of the Geant4 Software license.          *
+// ********************************************************************
+//
+//
+/// \file exampleB5.cc
+/// \brief Main program of the analysis/B5 example
+
+#include "globals.hh"
+#include "Randomize.hh"
+#include "time.h"
+
+// User Defined Detector
+#include "B5DetectorConstruction.hh"
+#include "B5PrimaryGeneratorAction.hh"
+#include "B5DetectorConstruction.hh"
+#include "B5ActionInitialization.hh"
+
+// Geant4
+#include "G4UImanager.hh"
+#include "G4RunManager.hh"
+#include "G4VModularPhysicsList.hh"
+#include "G4UImanager.hh"
+#include "G4StepLimiterPhysics.hh"
+#include "G4VisExecutive.hh"
+#include "G4UIExecutive.hh"
+
+// Physics list package
+#include "FTFP_BERT.hh"
+
+// Root
+#include "TFile.h"
+#include "TTree.h"
+#include "TString.h"
+#include "TSystem.h"
+#include "TRandom3.h"
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+// Global variables set by user
+bool gSaveStepLevel = false;    // 
+long gSeed = 0;                 // Random seed number. 0 for time seed 
+bool gUseGPS = true;            //
+
+// if gUseGPS = false, user defined beam conditions
+G4double gThetaLimitMin;  
+G4double gThetaLimitMax;
+G4double gBeamMomentum;
+G4String gParticle;
+
+
+int main(int argc,char** argv)
+{
+  if (argc != 1 && argc != 4){
+    std::cout << "./exampleB5 [1. Macro file name] [2. Output file name] [3. Random seed number]" << std::endl;
+    std::cout << "ex) ./exampleB5 run.mac example 1" << std::endl;
+    return 0;
+  }
+  
+  /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //                                      User Defined Parameters                                                //
+  
+  
+  gSaveStepLevel = false;  // whether save all the step information or not
+  gUseGPS = true;  // [true] : use General Particle Source described in your.mac file (/gps/position ...)   [false] : Random angle generation
+  if (gUseGPS == false){ // Random Beam Generation Setting
+    //                     // Uniform azimuthal angle [0 ~ 2pi]    
+    gThetaLimitMin = 0;    // polar angle min [deg]
+    gThetaLimitMax = 50;   // polar angle max [deg]
+    gBeamMomentum = 1000 * CLHEP::MeV; // Primary particle momentum
+    gParticle = "gamma";
+  }
+
+  //                                      User Defined Parameters                                                //
+  /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  
+  if (argc == 4){
+    //Random engine
+    CLHEP::HepRandom::setTheEngine(new CLHEP::RanecuEngine());
+    gSeed = (long) atol(argv[3]);
+    CLHEP::HepRandom::setTheSeed(gSeed);
+    gRandom -> SetSeed(gSeed);
+  }
+  
+  G4cout << "Save Step Level : " << gSaveStepLevel << G4endl;
+  if (gUseGPS)
+    G4cout << "Use GPS in .mac file" << G4endl;
+  else 
+    G4cout << "Random Theta Generation" << G4endl;
+  
+  TString str_fname = argv[2];
+  if (!str_fname.EndsWith(".root")){
+    str_fname += ".root";
+  }
+  else if (str_fname == ""){
+    str_fname = "VisMac.root";
+  }
+  
+  auto tr = new TTree("tree","Geant4 output");
+  
+  auto physicsList = new FTFP_BERT;
+  G4RunManager* runManager = new G4RunManager;
+  runManager -> SetUserInitialization(physicsList);
+  runManager -> SetUserInitialization(new B5ActionInitialization(tr));
+  runManager -> SetUserInitialization(new B5DetectorConstruction());
+  runManager -> SetUserAction(new B5PrimaryGeneratorAction());
+  runManager -> Initialize();
+  
+  TFile *tf = new TFile(str_fname,"RECREATE");
+  
+  G4VisManager* visManager = new G4VisExecutive;
+  visManager -> Initialize();
+  G4UImanager* UImanager = G4UImanager::GetUIpointer();
+  
+  if (argc != 1) {
+    
+    G4String command = "/control/execute ";
+    G4String fileName = argv[1];
+    UImanager -> ApplyCommand(command+fileName);
+    
+  }
+  else 
+  {
+    gUseGPS = true;
+    G4UIExecutive* ui = new G4UIExecutive(argc, argv);
+    UImanager -> ApplyCommand("/control/execute vis.mac"); 
+    ui -> SessionStart();
+    
+    delete ui;
+  }
+  
+  tf -> cd();
+  tr -> Write();
+  tf -> Close();
+
+  delete visManager;
+
+  return 0;
+}
---
+++ src/B5DetectorConstruction.cc
@@ -0,0 +1,252 @@
+//
+// ********************************************************************
+// * License and Disclaimer                                           *
+// *                                                                  *
+// * The  Geant4 software  is  copyright of the Copyright Holders  of *
+// * the Geant4 Collaboration.  It is provided  under  the terms  and *
+// * conditions of the Geant4 Software License,  included in the file *
+// * LICENSE and available at  http://cern.ch/geant4/license .  These *
+// * include a list of copyright holders.                             *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.  Please see the license in the file  LICENSE  and URL above *
+// * for the full disclaimer and the limitation of liability.         *
+// *                                                                  *
+// * This  code  implementation is the result of  the  scientific and *
+// * technical work of the GEANT4 collaboration.                      *
+// * By using,  copying,  modifying or  distributing the software (or *
+// * any work based  on the software)  you  agree  to acknowledge its *
+// * use  in  resulting  scientific  publications,  and indicate your *
+// * acceptance of all terms of the Geant4 Software license.          *
+// ********************************************************************
+//
+//
+/// \file B5DetectorConstruction.cc
+/// \brief Implementation of the B5DetectorConstruction class
+
+#include "B5DetectorConstruction.hh"
+#include "B5EmCalorimeterSD.hh"
+#include "B5LeadSD.hh"
+
+#include "G4TransportationManager.hh"
+
+#include "G4Material.hh"
+#include "G4Element.hh"
+#include "G4MaterialTable.hh"
+#include "G4NistManager.hh"
+
+#include "G4VSolid.hh"
+#include "G4Box.hh"
+#include "G4Tubs.hh"
+#include "G4LogicalVolume.hh"
+#include "G4VPhysicalVolume.hh"
+#include "G4PVPlacement.hh"
+#include "G4PVParameterised.hh"
+#include "G4PVReplica.hh"
+#include "G4UserLimits.hh"
+
+#include "G4SDManager.hh"
+#include "G4VSensitiveDetector.hh"
+#include "G4RunManager.hh"
+#include "G4GenericMessenger.hh"
+#include "G4tgbRotationMatrix.hh"
+
+#include "G4VisAttributes.hh"
+#include "G4Colour.hh"
+
+#include "G4ios.hh"
+#include "G4SystemOfUnits.hh"
+
+// Root classes
+#include "TString.h"
+    
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+int gNumberOfScintillators;
+B5DetectorConstruction::B5DetectorConstruction()
+  : G4VUserDetectorConstruction()
+{
+  NumberOfLayers = 110;
+  NumberOfScintillators = 270; // Number of scintillator segments in a layer
+  //NumberOfScintillators = 10; // Number of scintillator segments in a layer
+  
+  gNumberOfScintillators = NumberOfScintillators;
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+B5DetectorConstruction::~B5DetectorConstruction()
+{
+  
+  for (auto visAttributes: fVisAttributes) {
+    delete visAttributes;
+  }  
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+G4VPhysicalVolume* B5DetectorConstruction::Construct(){
+  G4NistManager* nist = G4NistManager::Instance();
+  
+  // -----------------------------------------------------
+  // World
+
+  G4Material* world_mat = nist -> FindOrBuildMaterial("G4_AIR");
+  G4double world_size = 5.*m;
+  
+  G4Box* solidWorld =    
+    new G4Box("World",                       // its name
+              0.5*world_size,                // half x
+              0.5*world_size,                // half y
+              0.5*world_size);               // half z
+  
+  G4LogicalVolume* logicWorld =                         
+    new G4LogicalVolume(solidWorld,          //its solid
+                        world_mat,           //its material
+                        "World");            //its name
+
+  G4VPhysicalVolume* physWorld = 
+    new G4PVPlacement(0,                     //no rotation
+                      G4ThreeVector(),       //at (0,0,0)
+                      logicWorld,            //its logical volume
+                      "World",               //its name
+                      0,                     //its mother  volume
+                      false,                 //no boolean operation
+                      0,                     //copy number
+                      false);                //overlaps checking
+
+
+  // -----------------------------------------------------
+
+  // Detector
+
+  // Tungsten plate
+  G4Material* detector_mat_W = nist -> FindOrBuildMaterial("G4_W");
+  G4double W_size_x = 27.*cm;
+  //G4double W_size_x = 1.*cm;
+  G4double W_size_y = W_size_x;
+  G4double W_size_z = 0.15 * mm; 
+  
+  // EJ 200 Scintillator
+  G4double scint_size_x = W_size_x;
+  G4double scint_size_y = 1.*mm;
+  G4double scint_size_z = 1.*mm;
+
+  G4double layer_gap_z = W_size_z + scint_size_z;
+  
+  G4Material* EJ200 = nist -> FindOrBuildMaterial("G4_PLASTIC_SC_VINYLTOLUENE");
+  
+  std::vector<G4double> vRot;
+  vRot.clear();
+  vRot.push_back(0.);vRot.push_back(-1.);vRot.push_back(0.);
+  vRot.push_back(1.);vRot.push_back(0.);vRot.push_back(0.);
+  vRot.push_back(0.);vRot.push_back(0.);vRot.push_back(1.);
+  
+  G4tgbRotationMatrix* RotBuilder = new G4tgbRotationMatrix();
+  G4RotationMatrix* pRot = (G4RotationMatrix*) RotBuilder->BuildG4RotMatrixFrom9(vRot); 
+  
+  
+  G4Box* solidW = new G4Box("W",
+			    0.5*W_size_x,
+			    0.5*W_size_y,
+			    0.5*W_size_z);
+  
+  G4Box* solidScint = new G4Box("Scint",
+				0.5*scint_size_x,
+				0.5*scint_size_y,
+				0.5*scint_size_z);
+
+  G4Box* solidScintPlate = new G4Box("ScintPlate",
+				     0.5*scint_size_x,
+				     0.5*scint_size_x,
+				     0.5*scint_size_z);
+
+  auto visAttributes_Scint = new G4VisAttributes(G4Colour(0.0,1.0,1.0));
+  
+  
+  for (int iLayer = 0; iLayer < NumberOfLayers; iLayer++){
+    G4double dLayer = (G4double) iLayer;
+    
+    G4double W_offset_z = 0.5 * W_size_z + layer_gap_z * dLayer;
+    G4double scint_offset_z = W_offset_z + 0.5 * layer_gap_z;
+    
+    TString str_leadname_tmp = Form("W%d",iLayer);
+    G4String str_leadname = str_leadname_tmp.Data();
+    
+    TString str_scintname_tmp = Form("Scint%d",iLayer);
+    G4String str_scintname = str_scintname_tmp.Data();
+    
+    G4LogicalVolume* logicScintPlate =  new G4LogicalVolume(solidScintPlate, EJ200, str_scintname);
+    G4LogicalVolume* logicScint =  new G4LogicalVolume(solidScint, EJ200, str_scintname); 
+    
+    if (iLayer % 2 == 0){
+      auto pPhysScint = new G4PVPlacement(0,
+					  G4ThreeVector(0,0,scint_offset_z),
+					  logicScintPlate,
+					  str_scintname,
+					  logicWorld,
+					  false,
+					  iLayer,
+					  false);
+      G4VPhysicalVolume* div_ = new G4PVReplica("div_", logicScint, pPhysScint, kYAxis, NumberOfScintillators, scint_size_y);
+    }
+    else {
+      auto pPhysScint = new G4PVPlacement(pRot,
+					  G4ThreeVector(0,0,scint_offset_z),
+					  logicScintPlate,
+					  str_scintname,
+					  logicWorld,
+					  false,
+					  iLayer,
+					  false);
+      G4VPhysicalVolume* div_ = new G4PVReplica("div_", logicScint, pPhysScint, kYAxis, NumberOfScintillators, scint_size_y);
+    }
+    
+    
+    G4LogicalVolume* logicW = new G4LogicalVolume(solidW, detector_mat_W, str_leadname);
+    new G4PVPlacement(0,
+		      G4ThreeVector(0,0,W_offset_z),
+		      logicW,
+		      str_leadname,
+		      logicWorld,
+		      false,
+		      0,
+		      false);
+    
+
+    // visualization attributes 
+    logicScintPlate->SetVisAttributes(visAttributes_Scint);
+    fVisAttributes.push_back(visAttributes_Scint);  
+  }
+  return physWorld;
+}
+
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+void B5DetectorConstruction::ConstructSDandField()
+{
+  //Sensitive Detector
+  for (int iLayer = 0 ; iLayer < NumberOfLayers; iLayer++){
+    TString str_leadname_tmp = Form("W%d",iLayer);
+    G4String str_leadname = str_leadname_tmp.Data();
+    G4String str_SDnameLead = (str_leadname_tmp+"_SD").Data();
+    
+    TString str_scintname_tmp = Form("Scint%d",iLayer);
+    G4String str_scintname = str_scintname_tmp.Data();
+    G4String str_SDnameScint = (str_scintname_tmp+"_SD").Data();
+    
+    B5EmCalorimeterSD* ScintillatorSD = new B5EmCalorimeterSD(str_SDnameScint,iLayer,0);
+    G4SDManager::GetSDMpointer() -> AddNewDetector(ScintillatorSD);
+    SetSensitiveDetector(str_scintname, ScintillatorSD, true);
+    
+    // W
+    B5LeadSD* LeadSD = new B5LeadSD(str_SDnameLead,iLayer);
+    G4SDManager::GetSDMpointer() -> AddNewDetector(LeadSD);
+    SetSensitiveDetector(str_leadname, LeadSD, true);
+  }
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
